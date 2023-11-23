aae94c6a745336ee9b4d104a8902705c0509244d
BWang2010 <bella.y.zhi@gmail.com>
2023-11-23 18:32:22 +0000

    added genes to individual person

--- individual.py
+++ individual.py
@@ -2,6 +2,11 @@ import random
 import string #All ascii characters
 class Individual:
     def __init__(self, idealGenes):
+
         alphabet = string.ascii_lowercase + " "
+        self.act_genes = ""
         for i in range(len(idealGenes)):
-            print(random.choice(alphabet))
+            a = random.choice(alphabet)
+            self.act_genes = self.act_genes + a
+
+
--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from individual import Individual
 if __name__ == "__main__":
     ideal_genes = "a cat and a mouse with cheese"
     Trav = Individual(ideal_genes)
+    print(Trav.act_genes)
+#TODO: CREATE RANDOM GENES (PROPERTIES) AND MAKE 2 INDIVIDUALS (MAKE SURE ALL WORKS AND PUSH AND PULL + SEND REQUEST TO GITHUB)
