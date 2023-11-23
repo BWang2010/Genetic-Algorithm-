229aff7b326101071208e92ef8c6fe5dec07af9e
BWang2010 <bella.y.zhi@gmail.com>
2023-11-23 18:39:34 +0000

    corrected fitness command

--- individual.py
+++ individual.py
@@ -2,11 +2,13 @@ import random
 import string #All ascii characters
 class Individual:
     def __init__(self, idealGenes):
-
         alphabet = string.ascii_lowercase + " "
         self.act_genes = ""
+        self.fitness = 0
         for i in range(len(idealGenes)):
             a = random.choice(alphabet)
             self.act_genes = self.act_genes + a
+            if a == idealGenes[i]:
+                self.fitness = self.fitness + 1
 
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 from individual import Individual
 if __name__ == "__main__":
-    ideal_genes = "a cat and a mouse with cheese"
+    ideal_genes = "mice and cats and cheese"
     Trav = Individual(ideal_genes)
     print(Trav.act_genes)
+    print(Trav.fitness)
 #TODO: CREATE RANDOM GENES (PROPERTIES) AND MAKE 2 INDIVIDUALS (MAKE SURE ALL WORKS AND PUSH AND PULL + SEND REQUEST TO GITHUB)
